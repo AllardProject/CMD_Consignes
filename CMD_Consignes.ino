41e8f533fdf632d01252673470d4f01378040a52
Trycarc <cavalli.hugo@laposte.net>
2020-04-10 17:21:29 +0200

    Release/

--- CMD_Consignes.ino
+++ CMD_Consignes.ino
@@ -1,31 +1,11 @@
 #include <Arduino.h>
 #include "lib/rs485/ArduinoRS485.h"  // ArduinoModbus depends on the ArduinoRS485 library
 #include "lib/ArduinoModbus.h"
-//test 2 
-const int P1 = 2; // broche 2 du micro-contrôleur se nomme maintenant : p1
-const int P2 = 3;
-const int P3 = 4;
-const int P4 = 5;
-const int P5 = 6;
-const int P6 = 7;
-const int P7 = 8;
-const int P8 = 9;
-const int P9 = 10;
-const int P10 = 11;
-const int P11 = 12;
-const int P12 = 13;
-int Led1;
-int Led2;
-int Led3;
-int Led4;
-int Ser1;
-int Ser2;
-int Ser3;
-int Ser4;
+
+const int ledPin = LED_BUILTIN;
 
 void setup() //fonction d'initialisation de la carte
 {
-	 Serial.begin(9600);
 
 	  Serial.println("Modbus RTU Server LED");
 
@@ -35,111 +15,37 @@ void setup() //fonction d'initialisation de la carte
 	    while (1);
 	  }
 
+	  // configure the LED
+	  pinMode(ledPin, OUTPUT);
+	  digitalWrite(ledPin, LOW);
+
 	  // configure a single coil at address 0x00
 	  ModbusRTUServer.configureCoils(0x00, 1);
 
 
-//contenu de l'initialisation
-pinMode(P1, OUTPUT);
-pinMode(P2, OUTPUT);
-pinMode(P3, OUTPUT);
-pinMode(P4, OUTPUT);
-pinMode(P5, OUTPUT);
-pinMode(P6, OUTPUT);
-pinMode(P7, OUTPUT);
-pinMode(P8, OUTPUT);
-pinMode(P9, OUTPUT);
-pinMode(P10, OUTPUT);
-pinMode(P11, OUTPUT);
-pinMode(P12, OUTPUT);
+
+
 }
 void loop() //fonction principale, elle se répète (s’exécute) à l'infini
 {
 
-	ModbusRTUServer.poll();
+	  // poll for Modbus RTU requests
+	  ModbusRTUServer.poll();
 
 	  // read the current value of the coil
 	  int coilValue = ModbusRTUServer.coilRead(0x00);
 
 	  if (coilValue) {
 	    // coil value set, turn LED on
-	    digitalWrite(P1, HIGH);
+	    digitalWrite(ledPin, HIGH);
+
+
 	  } else {
 	    // coild value clear, turn LED off
-	    digitalWrite(P2, HIGH);
+	    digitalWrite(ledPin, LOW);
+
 	  }
 
 
-	/*
-  Led1 = 1;
 
-  if (Led1 == 0){
-    digitalWrite(P1, HIGH); //allumer L1 rouge
-    digitalWrite(P2, LOW); // Eteindre L1 vert
-  }
-  if (Led1 == 1){
-      digitalWrite(P1, LOW); //Eteindre rouge
-      digitalWrite(P2, HIGH); // allumer  vert
-    }
-
-
-  Led2 = 1;
-
-  if (Led2 == 0){
-    digitalWrite(P3, HIGH); //allumer L1 rouge
-    digitalWrite(P4, LOW); // Eteindre L1 vert
-  }
-  if (Led2 == 1){
-      digitalWrite(P3, LOW); //Eteindre rouge
-      digitalWrite(P4, HIGH); // allumer  vert
-    }
- Led3 = 0;
-
-  if (Led3 == 0){
-    digitalWrite(P5, HIGH); //allumer L1 rouge
-    digitalWrite(P6, LOW); // Eteindre L1 vert
-  }
-  if (Led3 == 1){
-      digitalWrite(P5, LOW); //Eteindre rouge
-      digitalWrite(P6, HIGH); // allumer  vert
-    }
-
-
-  Led4 = 1;
-
-  if (Led4 == 0){
-    digitalWrite(P7, HIGH); //allumer L1 rouge
-    digitalWrite(P8, LOW); // Eteindre L1 vert
-  }
-  if (Led4 == 1){
-      digitalWrite(P7, LOW); //Eteindre rouge
-      digitalWrite(P8, HIGH); // allumer  vert
-    }
-
-  Ser1 = 1;
-
-  if(Ser1 == 1){
-    digitalWrite(P9, HIGH); //ouvrir serrure 1
-    }
-
-  Ser2 = 1;
-
-  if(Ser2 == 1){
-    digitalWrite(P10, HIGH);
-    }
-
-     Ser3 = 1;
-
-  if(Ser3 == 1){
-    digitalWrite(P11, HIGH);
-    }
-
-
-     Ser4 = 1;
-
-  if(Ser4 == 1){
-    digitalWrite(P12, HIGH);
-    }
-
-*/
 }
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2020-04-03 17:20:54
+//This file has been generated on 2020-04-10 17:17:30
 
 #include "Arduino.h"
 #include <Arduino.h>
