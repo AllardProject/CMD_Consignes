2eb6930932568923af44c2a81c0aa379dbb6915a
Trycarc <cavalli.hugo@laposte.net>
2020-04-16 10:12:12 +0200

    CMD_Consignes.ino

--- CMD_Consignes.ino
+++ CMD_Consignes.ino
@@ -2,7 +2,18 @@
 #include "lib/rs485/ArduinoRS485.h"  // ArduinoModbus depends on the ArduinoRS485 library
 #include "lib/ArduinoModbus.h"
 
-const int ledPin = LED_BUILTIN;
+const int P1 = 2; // broche 2 du micro-contrôleur se nomme maintenant : p1
+const int P2 = 3;
+const int P3 = 4;
+const int P4 = 5;
+const int P5 = 6;
+const int P6 = 7;
+const int P7 = 8;
+const int P8 = 9;
+const int P9 = 10;
+const int P10 = 11;
+const int P11 = 12;
+const int P12 = 13;
 
 void setup() //fonction d'initialisation de la carte
 {
@@ -16,11 +27,36 @@ void setup() //fonction d'initialisation de la carte
 	  }
 
 	  // configure the LED
-	  pinMode(ledPin, OUTPUT);
-	  digitalWrite(ledPin, LOW);
+	  pinMode(P1, OUTPUT);
+	  pinMode(P2, OUTPUT);
+	  pinMode(P3, OUTPUT);
+	  pinMode(P4, OUTPUT);
+	  pinMode(P5, OUTPUT);
+	  pinMode(P6, OUTPUT);
+	  pinMode(P7, OUTPUT);
+	  pinMode(P8, OUTPUT);
+	  pinMode(P9, OUTPUT);
+	  pinMode(P10, OUTPUT);
+	  pinMode(P11, OUTPUT);
+	  pinMode(P12, OUTPUT);
 
 	  // configure a single coil at address 0x00
 	  ModbusRTUServer.configureCoils(0x00, 1);
+	  ModbusRTUServer.configureCoils(0x01, 1);
+	  /*ModbusRTUServer.configureCoils(0x02, 1);
+	  ModbusRTUServer.configureCoils(0x03, 1);
+	  ModbusRTUServer.configureCoils(0x04, 1);
+	  ModbusRTUServer.configureCoils(0x05, 1);
+	  ModbusRTUServer.configureCoils(0x06, 1);
+	  ModbusRTUServer.configureCoils(0x07, 1);
+	  ModbusRTUServer.configureCoils(0x08, 1);
+	  ModbusRTUServer.configureCoils(0x09, 1);
+	  ModbusRTUServer.configureCoils(0x10, 1);
+	  ModbusRTUServer.configureCoils(0x11, 1);
+	  ModbusRTUServer.configureCoils(0x12, 1);
+	  ModbusRTUServer.configureCoils(0x13, 1);
+	  ModbusRTUServer.configureCoils(0x14, 1);
+	  ModbusRTUServer.configureCoils(0x15, 1);*/
 
 
 
@@ -33,19 +69,94 @@ void loop() //fonction principale, elle se répète (s’exécute) à l'infini
 	  ModbusRTUServer.poll();
 
 	  // read the current value of the coil
-	  int coilValue = ModbusRTUServer.coilRead(0x00);
+	  int coilValue0 = ModbusRTUServer.coilRead(0x00); //led
+	  int coilValue1 = ModbusRTUServer.coilRead(0x01); //gâche
+	  /*int coilValue2 = ModbusRTUServer.coilRead(0x02); //led
+	  int coilValue3 = ModbusRTUServer.coilRead(0x03); //gache
+	  int coilValue4 = ModbusRTUServer.coilRead(0x04);	//l
+	  int coilValue5 = ModbusRTUServer.coilRead(0x05);	//g
+	  int coilValue6 = ModbusRTUServer.coilRead(0x06);	//l
+	  int coilValue7 = ModbusRTUServer.coilRead(0x07);	//g
+	  int coilValue8 = ModbusRTUServer.coilRead(0x08); //l
+	  int coilValue9 = ModbusRTUServer.coilRead(0x09); //g
+	  int coilValue10 = ModbusRTUServer.coilRead(0x10);	//l
+	  int coilValue11 = ModbusRTUServer.coilRead(0x11);	//g
+	  int coilValue12 = ModbusRTUServer.coilRead(0x12);	//l
+	  int coilValue13 = ModbusRTUServer.coilRead(0x13);	//g
+	  int coilValue14 = ModbusRTUServer.coilRead(0x14);	//l
+	  int coilValue15 = ModbusRTUServer.coilRead(0x15);	//g*/
 
-	  if (coilValue) {
-	    // coil value set, turn LED on
-	    digitalWrite(ledPin, HIGH);
+	  if (coilValue0 == 0){
+	    digitalWrite(P1, HIGH); //allumer L1 rouge
+	    digitalWrite(P2, LOW); // Eteindre L1 vert
+	  } else{
+	      digitalWrite(P1, LOW); //Eteindre rouge
+	      digitalWrite(P2, HIGH); // allumer  vert
+	    }
 
 
-	  } else {
-	    // coild value clear, turn LED off
-	    digitalWrite(ledPin, LOW);
+
+
+
+	  if (coilValue1 == 1){
+	    digitalWrite(P3, HIGH); //allumer ser
 
 	  }
+	  Serial.println(coilValue0);
+	  Serial.println(coilValue1);
 
+	  /*
+	  if (Led2 == 1){
+	      digitalWrite(P3, LOW); //Eteindre rouge
+	      digitalWrite(P4, HIGH); // allumer  vert
+	    }
+	 Led3 = 0;
+
+	  if (Led3 == 0){
+	    digitalWrite(P5, HIGH); //allumer L1 rouge
+	    digitalWrite(P6, LOW); // Eteindre L1 vert
+	  }
+	  if (Led3 == 1){
+	      digitalWrite(P5, LOW); //Eteindre rouge
+	      digitalWrite(P6, HIGH); // allumer  vert
+	    }
+
+
+	  Led4 = 1;
+
+	  if (Led4 == 0){
+	    digitalWrite(P7, HIGH); //allumer L1 rouge
+	    digitalWrite(P8, LOW); // Eteindre L1 vert
+	  }
+	  if (Led4 == 1){
+	      digitalWrite(P7, LOW); //Eteindre rouge
+	      digitalWrite(P8, HIGH); // allumer  vert
+	    }
+
+	  Ser1 = 1;
+
+	  if(Ser1 == 1){
+	    digitalWrite(P9, HIGH); //ouvrir serrure 1
+	    }
+
+	  Ser2 = 1;
+
+	  if(Ser2 == 1){
+	    digitalWrite(P10, HIGH);
+	    }
+
+	     Ser3 = 1;
+
+	  if(Ser3 == 1){
+	    digitalWrite(P11, HIGH);
+	    }
+
+
+	     Ser4 = 1;
+
+	  if(Ser4 == 1){
+	    digitalWrite(P12, HIGH);
+	    }*/
 
 
 }
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2020-04-10 17:17:30
+//This file has been generated on 2020-04-16 10:09:03
 
 #include "Arduino.h"
 #include <Arduino.h>
